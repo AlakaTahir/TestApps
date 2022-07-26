ff0eade93e893f14dd527016c9fedb2cf012491b
AlakaTahir <olawaletech2@gmail.com>
2022-07-26 09:56:35 +0100

    Test Applications solution

---
+++ Test.Logic/DivisionLogic.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Test.Model;
+
+namespace Test.Logic
+{
+    public class DivisionLogic
+    {
+        public double Divide(DivisionModel model)
+        {
+            return model.NumA / model.NumB;
+        }
+    }
+}
---
+++ Test.Model/DivisionModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Test.Model
+{
+    public class DivisionModel
+    {
+        public double NumA { get;set; }
+        public double NumB { get;set; }
+    }
+}
